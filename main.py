6fc77f6e4351f02aac65ee06ae667dd8b44ffe32
gahan-quixom <gahan@quixom.com>
2017-04-20 18:58:26 +0530

    to be done : fix global variable VIDEOS manipulation

--- main.py
+++ main.py
@@ -17,50 +17,38 @@ import xbmcplugin
 _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
+_page_number = 0
 
 # Get Url
-page_num = 0
-LIST_MOVIES_JSON = "http://yts.ag/api/v2/list_movies.json?page_number="+str(page_num)
-# base_url = "https://yts.ag/api/v2/list_movies.json"
-# uri = ''
-opt_qualities = ['720p', '1080p', '3D', 'ALL']
-opt_ratings = range(10)
-opt_sorts = ['date', 'seeds', 'size', 'alphabet', 'rating']
-opt_orders = ['desc', 'asc']
-
-movie_list_params = {
-    'limit': 20,  # Maximum number of returned items
-    'set': 1,  # Which set (page) do you want to return?
-    'quality': 'ALL',  # {720p, 1080p, 3D, ALL}
-    'rating': 0,  # Minimum rating between 0 - 9
-    'keywords': '',  # {String}
-    'genre': 'ALL',  # {String} Refer to http://www.imdb.com/genre/
-    'sort': 'date',  # {date, seeds, size, alphabet, rating}
-    'order': 'desc'  # {desc, asc}
-}
+BASE_URL = "http://yts.ag/api/v2/list_movies.json"
+_URI = "http://yts.ag/api/v2/list_movies.json"
 
 VIDEOS = {}
 
 
-def fetch_data(url):
-    """
-    Fetch Json file and return as python list if response is 200
-x[]
-    :param url: json url
-    :return: list if success else None
-    """
-    get_url_response = requests.get(url)
+def fetch_data(_URI, _page_number):
+    xbmc.log('fetching json file..............', 2)
+    url_to_fetch = _URI + '?page_number=' + str(_page_number)
+    xbmc.log('fetching url : ' + url_to_fetch, 2)
+    get_url_response = requests.get(url_to_fetch)
     if get_url_response.status_code == 200:
         return json.loads(get_url_response.text)
     else:
         return None
 
 
-xbmc.log("got url")
-json_data = fetch_data(LIST_MOVIES_JSON)
-if json_data is not None:
-    VIDEOS['movies'] = json_data['data']['movies']
-xbmc.log("got data")
+def load_page():
+    xbmc.log('loading page..........', 2)
+
+    if _URI == BASE_URL:
+        this_page = _page_number
+    else:
+        this_page = _page_number + 1
+    json_data = fetch_data(_URI, this_page)
+    xbmc.log('type of json_data: ' + str(type(json_data)), 2)
+    if json_data is not None:
+        VIDEOS['movies'] = json_data['data']['movies']
+        # xbmc.log('listing length: ' + str(VIDEOS.keys()), 2)
 
 
 def get_url(**kwargs):
@@ -72,6 +60,7 @@ def get_url(**kwargs):
     :return: plugin call URL
     :rtype: str
     """
+    xbmc.log('formating url.......', 2)
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
 
@@ -89,6 +78,7 @@ def get_categories():
     :return: The list of video categories
     :rtype: list
     """
+    xbmc.log('Retriving categories', 2)
     return VIDEOS.iterkeys()
 
 
@@ -107,7 +97,12 @@ def get_videos(category):
     :return: the list of videos in the category
     :rtype: list
     """
-    return VIDEOS[category]
+    xbmc.log('Retriving movie list.............', 2)
+    xbmc.log('Category is .............' + category, 2)
+    xbmc.log('Retriving movie list.............', 2)
+    xbmc.log('type of videos' + str(VIDEOS), 2)
+    # xbmc.log('type of videos' + str(len(VIDEOS[category])), 2)
+    # return VIDEOS[category]
 
 
 def list_categories():
@@ -115,6 +110,9 @@ def list_categories():
     Create the list of video categories in the Kodi interface.
     """
     # Get video categories
+    xbmc.log('listing categories................', 2)
+
+    load_page()
     categories = get_categories()
     # Iterate through categories
     for category in categories:
@@ -145,6 +143,7 @@ def list_categories():
         # Add our item to the Kodi virtual folder listing.
         xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    # xbmcplugin.addDirectoryItem(_handle, url='{0}?action=load&page_number={1}'.format(_url, _page_number), liz=xbmcgui.ListItem(label='more....'), is_folder=True)
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
     xbmcplugin.endOfDirectory(_handle)
@@ -165,13 +164,16 @@ def list_videos(category):
     # panel = self.getControl(123)
     for video in videos:
         xbmc.log('in listing videos loop////////', 2)
+        xbmc.log(video['title'], 2)
+        xbmc.log(video['title'], 2)
+        xbmc.log(video['title'], 2)
         # Create a list item with a text label and a thumbnail image.
         list_item = xbmcgui.ListItem(label=video['title'])
-        list_item.setArt({'thumb': video['medium_cover_image'], 'icon': video['small_cover_image'], 'fanart': video['background_image']})
+        # list_item.setArt({'thumb': video['medium_cover_image'], 'icon': video['small_cover_image'], 'fanart': video['background_image']})
         # Set 'IsPlayable' property to 'true'. This is mandatory for playable items!
         # list_item.setProperty('IsPlayable', 'true')
         list_item.setProperty('fanart_image', video['background_image'])
-        listing.append((url, list_item, is_folder))
+        # listing.append((url, list_item, is_folder))
         list_item.addContextMenuItems([('Refresh', 'Container.Refresh'),
                                        ('Details', 'ActivateWindow(movieinformation)'),
                                        ('Go up', 'Action(ParentDir)')])
@@ -186,8 +188,8 @@ def list_videos(category):
         xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         # xbmcplugin.addDirectoryItem(_handle, url, listing, len(listing))
     # panel.addItems(listing)
-    next_page = xbmcgui.ListItem(label='more...')
-    xbmcplugin.addDirectoryItem(_handle, next_page, 'Container.Refresh')
+    # next_page = xbmcgui.ListItem(label='more...')
+    # xbmcplugin.addDirectoryItem(_handle, next_page, 'Container.Refresh')
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
@@ -221,14 +223,6 @@ def list_details(movie_url):
     xbmcplugin.endOfDirectory(_handle)
 
 
-def load_next_page(page_number):
-    if page_num is 0:
-        page_number = page_num
-    else:
-        page_number += 1
-    url = '{0}?action=listing&page_number={1}'.format(_url, page_number)
-
-
 def router(paramstring):
     """
     Router function that calls other functions depending on the provided paramstring
@@ -249,8 +243,8 @@ def router(paramstring):
             list_details(params['video'])
             # xbmc.log('log')
             # xbmc.executebuiltin('ActivateWindow(movieinformation)')
-        elif params['action'] == 'next_page':
-            load_next_page(params['page_number'])
+        elif params['action'] == 'load':
+            load_page(params['page_number'])
         else:
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
