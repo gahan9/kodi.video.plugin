6dc6b175fb5d1d9aba11d339d30ec2966590a002
gahan-quixom <gahan@quixom.com>
2017-04-21 19:00:17 +0530

    added single function to create list item

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import xbmcplugin
 # from dialogs.DialogMovieInfo import DialogMovieInfo
 
 # Get the plugin url in plugin:// notation.
-_url = sys.argv[0]
+# _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
 
@@ -23,192 +23,80 @@ BASE_URL = "http://yts.ag/api/v2/list_movies.json?page=1"
 _URI = ""
 
 VIDEOS = {}
-THIS_PAGE = {'page': 1}
 
 
 def fetch_data(page):
     xbmc.log('In FUN=fetch_data()  **********', 2)
-    xbmc.log('fetching json file..............', 2)
-    if page is None:
-        page = 1
-    else:
-        pass
+    # xbmc.log('fetching json file..............', 2)
     _URI = BASE_URL.split("=")[0] + "=" + str(page)
+    xbmc.log('page_yield() : ' + str(page), 2)
     xbmc.log('fetching url : ' + _URI, 2)
     get_url_response = requests.get(_URI)
     if get_url_response.status_code == 200:
-        THIS_PAGE['page'] = int(json.loads(get_url_response.text)['data']['page_number'])
         return json.loads(get_url_response.text)
     else:
         return None
 
 
-def load_page():
+def load_page(page_num):
     xbmc.log('In FUN=load_page() **********', 2)
-    # page_num = _URI.split("=")[-1]
     xbmc.log('URI : ' + _URI, 2)
-    xbmc.log('page num : ' + str(THIS_PAGE['page']), 2)
-    page_num = int(THIS_PAGE['page']) + 1
-    xbmc.log('Current page number : ', page_num)
     json_data = fetch_data(page_num)
-    xbmc.log('')
     xbmc.log('type of json_data: ' + str(type(json_data)), 2)
     if json_data is not None:
         VIDEOS['movies'] = json_data['data']['movies']
-        THIS_PAGE['page'] = page_num
-        # xbmc.log('listing length: ' + str(VIDEOS.keys()), 2)
-
-
-def get_url(**kwargs):
-    """
-    Create a URL for calling the plugin recursively from the given set of keyword arguments.
-
-    :param kwargs: "argument=value" pairs
-    :type kwargs: dict
-    :return: plugin call URL
-    :rtype: str
-    """
-    xbmc.log('In FUN=get_url()  **********', 2)
-    # xbmc.log('formating url.......', 2)
-    return '{0}?{1}'.format(_url, urlencode(kwargs))
 
 
 def get_categories():
-    """
-    Get the list of video categories.
-
-    Here you can insert some parsing code that retrieves
-    the list of video categories (e.g. 'Movies', 'TV-shows', 'Documentaries' etc.)
-    from some site or server.
-
-    .. note:: Consider using `generator functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :return: The list of video categories
-    :rtype: list
-    """
     xbmc.log('In FUN=get_categories()  **********', 2)
-    # xbmc.log('Retriving categories', 2)
     return VIDEOS.iterkeys()
 
 
-def get_videos(category):
-    """
-    Get the list of videofiles/streams.
-
-    Here you can insert some parsing code that retrieves
-    the list of video streams in the given category from some site or server.
-
-    .. note:: Consider using `generators functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :param category: Category name
-    :type category: str
-    :return: the list of videos in the category
-    :rtype: list
-    """
-    xbmc.log('In FUN=get_videos()  **********', 2)
-    # xbmc.log('Retriving movie list.............', 2)
-    # xbmc.log('Category is .............' + category, 2)
-    # xbmc.log('Retriving movie list.............', 2)
-    # xbmc.log('type of videos' + str(len(VIDEOS)), 2)
-    return VIDEOS[category]
-
-
 def list_categories():
-    """
-    Create the list of video categories in the Kodi interface.
-    """
-    # Get video categories
     xbmc.log('In FUN=list_categories()  **********', 2)
-    # xbmc.log('listing categories................', 2)
-    load_page()
+    load_page(1)
     categories = get_categories()
-    # Iterate through categories
+    xbmc.log('categories : ' + str(categories), 2)
     for category in categories:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category, iconImage=VIDEOS[category][0]['small_cover_image'])
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': VIDEOS[category][0]['medium_cover_image'],
-                          'icon': VIDEOS[category][0]['small_cover_image'],
-                          'fanart': VIDEOS[category][0]['background_image_original']})
-        # Set additional info for the list item.
-        # setInfo allows to set various information for an item.
-        list_item.setInfo('video', {'name': VIDEOS[category][0]['title'],
-                                    'genre': VIDEOS[category][0]['genres'][0],
-                                    'rating': VIDEOS[category][0]['rating'],
-                                    'release_year': VIDEOS[category][0]['year'],
-                                    'details': VIDEOS[category][0]['description_full']})
-        list_item.addContextMenuItems([('Refresh', 'Container.Refresh'),
-                                       ('Go up', 'Action(ParentDir)')])
-        list_item.setProperty('fanart_image', VIDEOS[category][0]['background_image_original'])
-        # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=listing&category=Animals
-        url = get_url(action='listing', category=category)
-        # url = '{0}?action=listing&category={1}'.format(_url, category)
-        # is_folder = True means that this item opens a sub-list of lower level items.
-        is_folder = True
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_handle)
+        xbmc.log('************************** - - - - : - - ' + str(VIDEOS[category]), 2)
+        main_list('Movies', VIDEOS[category][0]['medium_cover_image'], VIDEOS[category][0]['background_image_original'], 'listing', VIDEOS[category][0]['url'], isFolder=True)
 
 
-def list_videos(category):
-    """
-    Create the list of playable videos in the Kodi interface.
-
-    :param category: Category name
-    :type category: str
-    """
+def list_videos(page):
     xbmc.log('In FUN=list_videos()  **********', 2)
-    # Get the list of videos in the category.
-    # xbmc.log('in listing videos*******************************', 2)
-    load_page()
-    videos = get_videos(category)
-    # Iterate through videos.
-    # panel = self.getControl(123)
-    for video in videos:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=video['title'])
-        list_item.setArt({'thumb': video['medium_cover_image'], 'icon': video['small_cover_image'], 'fanart': video['background_image']})
-        # Set 'IsPlayable' property to 'true'. This is mandatory for playable items!
-        # list_item.setProperty('IsPlayable', 'true')
-        list_item.setProperty('fanart_image', video['background_image'])
-        # listing.append((url, list_item, is_folder))
-        list_item.addContextMenuItems([('Refresh', 'Container.Refresh'),
-                                       ('Details', 'ActivateWindow(movieinformation)'),
-                                       ('Go up', 'Action(ParentDir)')])
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/vids/crab.mp4
-        # url = get_url(action='play', video=video['video'])
-        url = get_url(action='show', video=video['url'])
-        # url = '{0}?action=listing&video={1}'.format(_url, video['url'])
-        # Add the list item to a virtual Kodi folder.
-        # is_folder = False means that this item won't open any sub-list.
-        is_folder = True
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-        # xbmcplugin.addDirectoryItem(_handle, url, listing, len(listing))
-    # panel.addItems(listing)
-    # next_page = xbmcgui.ListItem(label='more...')
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.addDirectoryItem(_handle, url='{0}?action=load&video={1}'.format(_url, category), listitem=xbmcgui.ListItem(label='More >>>'), isFolder=True)
-    xbmcplugin.endOfDirectory(_handle)
+    page += 1
+    load_page(page)
+    movie_name = VIDEOS['movies']
+    for video in movie_name:
+        main_list(video['title_long'], video['medium_cover_image'], video['background_image'], 'show', video['url'], isFolder=True)
+    # xbmc.log("my data........" + str(VIDEOS['movies']), 2)
+    # xbmc.log('videos : ' + str(videos), 2)
+
+    xbmcplugin.addDirectoryItem(handle=abs(int(sys.argv[1])), url='{0}?action=load&page_num={1}'.format('plugin://plugin.video.example/', ), listitem=xbmcgui.ListItem(label='More >>>'), isFolder=True)
+
+
+def main_list(name, thumb, fanart, mode, path, isFolder=False):
+    # xbmc.log('IN FUN=main_list()......', 2)
+    # xbmc.log('name : ' + str(path), 2)
+    # xbmc.log('name : ' + str(urllib.unquote_plus(path)), 2)
+    u = "plugin://plugin.video.example/"
+    u += "?name=" + str(name)
+    u += "&action=" + str(mode)
+    u += "&url=" + str(urllib.url2pathname(path))
+    u += "&fanart=" + str(urllib.url2pathname(fanart))
+    u += "&thumb=" + str(urllib.url2pathname(thumb))
+    # xbmc.log('url' + str(u), 2)
+    liz = xbmcgui.ListItem(label=name, iconImage="", thumbnailImage=thumb)
+    liz.setInfo(type="video", infoLabels={"label": name, "title": name})
+    liz.addContextMenuItems([('Refresh', 'Container.Refresh'),
+                             # ('Details', 'ActivateWindow(movieinformation)'),
+                             ('Go up', 'Action(ParentDir)')])
+    liz.setProperty("fanart", fanart)
+    liz.setArt({'poster': thumb, 'fanart': fanart})
+    xbmcplugin.addDirectoryItem(handle=abs(int(sys.argv[1])), url=u, listitem=liz, isFolder=True)
 
 
 def list_details(movie_url):
-    """
-    scrap details of movie for a given url
-
-    :param movie_url: url of movie web page
-    :return: load xbmc page of deails of movie from param
-    """
     xbmc.log('In FUN=list_details()  **********', 2)
     movie_details = []
     movie_page_request = requests.get(movie_url)
@@ -224,8 +112,6 @@ def list_details(movie_url):
     movie_details.append(movie_genre)
     movie_details.append(movie_rating)
     movie_details.append(movie_year)
-    # # return xbmc.executebuiltin('ActivateWindow(movieinformation)')
-    # return xbmcgui.Dialog().ok(movie_name, movie_year, movie_genre, movie_rating)
     for info in movie_details:
         list_item = xbmcgui.ListItem(label=info)
         is_folder = False
@@ -236,49 +122,49 @@ def list_details(movie_url):
     xbmcplugin.endOfDirectory(_handle)
 
 
-def router(paramstring):
-    """
-    Router function that calls other functions depending on the provided paramstring
 
-    :param paramstring: URL encoded plugin paramstring
-    :type paramstring: str
-    """
+def router():
     # Parse a URL-encoded paramstring to the dictionary of
     # {<parameter>: <value>} elements
-    params = dict(parse_qsl(paramstring))
-    # Check the parameters passed to the plugin
-    if params:
-        if params['action'] == 'listing':
-            # Display the list of videos in a provided category.
+    param = []
+    paramstring = sys.argv[2]
+    if len(paramstring) >= 2:
+        params = sys.argv[2]
+        cleanedparams = params.replace('?', '')
+        if params[len(params) - 1] == '/':
+            params = params[0:len(params) - 2]
+        pairsofparams = cleanedparams.split('&')
+        param = {}
+        for i in range(len(pairsofparams)):
+            splitparams = pairsofparams[i].split('=')
+            if (len(splitparams)) == 2:
+                param[splitparams[0]] = splitparams[1]
+
+    xbmc.log('ALL_PARAMS : ' + str(param), 2)
+    if param:
+        if param['action'] == 'listing':
+            # xbmc.log("----------------------here" + str(param['movies']), 2)
             xbmc.log('Param : listing videos', 2)
-            list_videos(params['category'])
+            list_videos()
             xbmc.log('Param : listing videos successful', 2)
-        elif params['action'] == 'show':
-            # Play a video from a provided URL.
+        elif param['action'] == 'show':
             xbmc.log('Param : listing details', 2)
-            list_details(params['video'])
+            list_details(urllib.url2pathname(param['url']))
             xbmc.log('Param : listing details successful ', 2)
-            # xbmc.executebuiltin('ActivateWindow(movieinformation)')
-        elif params['action'] == 'load':
+        elif param['action'] == 'load':
             xbmc.log('Param : loading page ', 2)
-            list_videos(params['video'])
+            list_videos()
             xbmc.log('Param : loading page successful!', 2)
         else:
-            # If the provided paramstring does not contain a supported action
-            # we raise an exception. This helps to catch coding errors,
-            # e.g. typos in action names.
             xbmc.log('Param : value error', 2)
             raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
-        # If the plugin is called from Kodi UI without any parameters,
-        # display the list of video categories
         xbmc.log('Param : listing categories', 2)
         list_categories()
         xbmc.log('Param : listing categories successful', 2)
 
 
 if __name__ == '__main__':
-    # Call the router function and pass the plugin call parameters to it.
-    # We use string slicing to trim the leading '?' from the plugin call paramstring
-    router(sys.argv[2][1:])
-    # list_categories()
+    # router(sys.argv[2][1:])
+    router()
+    xbmcplugin.endOfDirectory(_handle,  cacheToDisc=False)
